044062f7a21e97c30b7e0ba896a2a0653e16132c
tr00per92 <zvezdo@mail.bg>
2014-09-25 21:09:04 +0300

    other types homework update

--- OOP/OOP-03-Other-Types/02-FractionCalculator/Fraction.cs
+++ OOP/OOP-03-Other-Types/02-FractionCalculator/Fraction.cs
@@ -18,6 +18,7 @@ namespace _02_FractionCalculator
             get { return this.numerator; }
             set { this.numerator = value; }
         }
+
         public long Denominator
         {
             get { return this.denominator; }
@@ -33,15 +34,22 @@ namespace _02_FractionCalculator
 
         public static Fraction operator -(Fraction f1, Fraction f2)
         {
-            long numerator = f1.numerator * f2.denominator - f2.numerator * f1.denominator;
-            long denomominator = f1.denominator * f2.denominator;
-            return new Fraction(numerator, denomominator);
+            checked
+            {
+                long numerator = f1.numerator * f2.denominator - f2.numerator * f1.denominator;
+                long denomominator = f1.denominator * f2.denominator;
+                return new Fraction(numerator, denomominator);
+            }            
         }
+
         public static Fraction operator +(Fraction f1, Fraction f2)
         {
-            long numerator = f1.numerator * f2.denominator + f2.numerator * f1.denominator;
-            long denomominator = f1.denominator * f2.denominator;
-            return new Fraction(numerator, denomominator);
+            checked
+            {
+                long numerator = f1.numerator * f2.denominator + f2.numerator * f1.denominator;
+                long denomominator = f1.denominator * f2.denominator;
+                return new Fraction(numerator, denomominator);
+            }            
         }
 
         public override string ToString()
