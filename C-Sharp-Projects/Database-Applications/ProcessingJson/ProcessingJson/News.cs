e651579a11278a536c2fbd24a29b70154829ef98
tr00per92 <zvezdo@mail.bg>
2016-03-07 19:17:17 +0200

    added processing json homework

---
+++ C-Sharp-Projects/Database-Applications/ProcessingJson/ProcessingJson/News.cs
@@ -0,0 +1,11 @@
+﻿namespace ProcessingJson
+{
+    public class News
+    {
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public string Link { get; set; }
+    }
+}
---
+++ C-Sharp-Projects/Database-Applications/ProcessingJson/ProcessingJson/Program.cs
@@ -0,0 +1,75 @@
+﻿namespace ProcessingJson
+{
+    using System;
+    using System.Net;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.IO;
+    using System.Web.UI;
+    using System.Xml.Linq;
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Linq;
+
+    public class Program
+    {
+        private const string FeedPath = "../../feed.xml";
+
+        public static void Main()
+        {
+            using (var webClient = new WebClient())
+            {
+                webClient.DownloadFile("https://softuni.bg/Feed/News", FeedPath);
+            }
+
+            var json = JsonConvert.SerializeXNode(XDocument.Load(FeedPath));
+            var jsonObject = JObject.Parse(json);
+
+            var items = jsonObject["rss"]["channel"]["item"];
+            var itemTitles = items.Select(x => x["title"]);
+            foreach (var title in itemTitles)
+            {
+                Console.WriteLine(title);
+            }
+
+            var news = JsonConvert.DeserializeObject<IEnumerable<News>>(items.ToString());
+            SaveNewsAsHtml(news, "../../news.html");
+        }
+
+        private static void SaveNewsAsHtml(IEnumerable<News> news, string htmlPath)
+        {
+            using (var streamWriter = new StreamWriter(htmlPath))
+            {
+                using (var writer = new HtmlTextWriter(streamWriter))
+                {
+                    writer.Write("<!DOCTYPE html>");
+                    writer.RenderBeginTag(HtmlTextWriterTag.Html);
+                    writer.Write("<head><meta charset=\"UTF-8\"></head>");
+
+                    writer.RenderBeginTag(HtmlTextWriterTag.Body);
+                    foreach (var item in news)
+                    {
+                        writer.RenderBeginTag(HtmlTextWriterTag.Div);
+
+                        writer.RenderBeginTag(HtmlTextWriterTag.H2);
+                        writer.Write(item.Title);
+                        writer.RenderEndTag();
+
+                        writer.AddAttribute(HtmlTextWriterAttribute.Href, item.Link);
+                        writer.RenderBeginTag(HtmlTextWriterTag.A);
+                        writer.Write(item.Link);
+                        writer.RenderEndTag();
+
+                        writer.RenderBeginTag(HtmlTextWriterTag.P);
+                        writer.Write(item.Description);
+                        writer.RenderEndTag();
+
+                        writer.RenderEndTag();
+                    }
+
+                    writer.RenderEndTag();
+                    writer.RenderEndTag();
+                }
+            }
+        }
+    }
+}
